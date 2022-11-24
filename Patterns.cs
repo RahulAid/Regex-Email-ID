b9e30295445746a7e151d74879f77741f31171b3
RahulAid <raccess97@gmail.com>
2022-11-24 12:50:08 +0530

    Email can handle optional part xyz. Only valid special characters proceeding to xyz are _,+,-,.

--- Patterns.cs
+++ Patterns.cs
@@ -8,8 +8,9 @@ public class Patterns
         // part1: must contain abc
         // part2: must contain @ and bridgelabz
         // part3: must contain dot after bridgelabz followed by co
+        // part4: handle optional part xyz. only valid special characters proceeding to xyz are _,+,-,.
 
-        var regex = new Regex(@"^([a][b][c])\@([b][r][i][d][g][e][l][a][b][z])\.([c][o])");
+        var regex = new Regex(@"^([a][b][c])([_\+\-\.])([a-zA-Z0-9]+)\@([b][r][i][d][g][e][l][a][b][z])\.([c][o])*$");
         bool matchRes = regex.IsMatch(email);
         if (matchRes == true)
         {
--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Email Validation Using Regex");
 
             Patterns objPattern = new Patterns();
-            objPattern.ValidateEMail("abc@bridgelabz.co");
+            objPattern.ValidateEMail("abc.xyz@bridgelabz.co");
         }
     }
 }
