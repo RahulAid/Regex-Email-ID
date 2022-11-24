f4e58e47b15224e5d65e8a317e161da4aeb84d08
RahulAid <raccess97@gmail.com>
2022-11-24 12:21:31 +0530

    Email must contain abc

---
+++ Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Patterns
+{
+    public void ValidateEMail(string email)
+    {       
+        var regex = new Regex(@"^([a][b][c])");               // abc mandatory check
+        bool matchRes = regex.IsMatch(email);
+        if (matchRes == true)
+        {
+            Console.WriteLine($"\n{email} is valid Email ID");
+        }
+        else
+        {
+            Console.WriteLine($"\n{email} is an invalid Email ID");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Regex_Email_ID
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Email Validation Using Regex");
+
+            Patterns objPattern = new Patterns();
+            objPattern.ValidateEMail("abc");
+        }
+    }
+}
